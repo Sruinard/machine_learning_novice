1d39a4d60765aa4cb9b551885c77433bb74970f5
Sruinard <41334908+Sruinard@users.noreply.github.com>
2022-08-25 07:06:02 +0000

    reorganize tutorials.

--- inference.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-m = tf.saved_model.load("./introduction_to_deep_learning_with_jax/lunar_lander_model")
-out = m.signatures["serving_default"](x=tf.ones((2, 8), dtype=tf.float32))
-print(out)
